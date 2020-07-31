3186251d74230080a697a5ab8ed841e2cf528c51
yingliyu <404997046@qq.com>
2020-07-31 18:39:01 +0800

    init graph

--- src/components/graph/index.tsx
+++ src/components/graph/index.tsx
@@ -1,7 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import css from './index.module.less';
 import graphData from './data';
 import * as d3 from 'd3';
+
+type abc = string | number | boolean | null | 'true';
 
 interface IGraphProps {
   title?: string;
@@ -9,22 +11,19 @@ interface IGraphProps {
 
 const Graph: React.FC<IGraphProps> = (props: IGraphProps) => {
   type INode = d3.SimulationNodeDatum & {
-    id: string | number;
-    code: string | number;
+    id: number;
+    code: number;
     name: string;
     type: string;
     level: number;
     colorIdx: number;
     keyword: string;
-    layout?: {};
-    layoutList?: {};
+    // layout?: {};
+    // layoutList?: {};
   };
 
   type ILink = d3.SimulationLinkDatum<INode> & {
-    id: string | number;
-    source: number;
-    target: number;
-    label: string;
+    label?: string;
   };
 
   const nodesData: INode[] = graphData.nodes;
@@ -34,18 +33,19 @@ const Graph: React.FC<IGraphProps> = (props: IGraphProps) => {
     initSvg();
     initForceSimulation();
   }, []);
-
+  const svgData = {
+    top: 20,
+    right: 20,
+    bottom: 20,
+    left: 20,
+    svgW: 800,
+    svgH: 800,
+    colorList: ['#967adc', '#8cc152', '#3bafda', '#f6bb42', '#37bc9b', '#ff7e90', '#ff7043'],
+    rediusList: [100, 70, 60, 40, 25],
+    fontSizeList: [26, 24, 20, 20, 20, 15],
+  };
   const initSvg = (): void => {
-    const svgData = {
-      top: 20,
-      right: 20,
-      bottom: 20,
-      left: 20,
-      svgW: 600,
-      svgH: 600,
-    };
-    const svgContainer = d3
-      .select('#graph-container')
+    d3.select('#graph-container')
       .append('svg')
       .attr('viewBox', '-100 -200 1300 1105')
       .attr('width', svgData.svgW)
@@ -55,91 +55,77 @@ const Graph: React.FC<IGraphProps> = (props: IGraphProps) => {
   };
 
   const initForceSimulation = () => {
-    // 声明一个力导向图
-    const simulation = d3.forceSimulation<INode, ILink>(nodesData);
-    // 手动调用 tick 使布局达到稳定状态
-    for (
-      let i = 0,
-        n = Math.ceil(Math.log(simulation.alphaMin()) / Math.log(1 - simulation.alphaDecay()));
-      i < n;
-      ++i
-    ) {
-      simulation.tick();
-    }
-    //     .alpha(2) // 活力，渲染之后再自动动多久
-    //     .force(
-    //       'link',
-    //       d3
-    //         .forceLink()
-    //         .id(data => {
-    //           // return data.code;
-    //           return '';
-    //         })
-    //         .distance(data => {
-    //           // 无分支的节点
-    //           console.log(data);
-    //           // if (data.target.name === '荣誉' || data.target.name === '组织') {
-    //           //   return 200;
-    //           // } else {
-    //           //   return data.target.level === 5 ? data.target.level * 22 : data.target.level * 3;
-    //           // }
-    //           return 200;
-    //         }),
-    //     ) // 映射id & 线的长度
-    //     .force(
-    //       'charge',
-    //       d3
-    //         .forceManyBody()
-    //         // .strength(d => -(9 - d.level) * 30)
-    //         .theta(0.01)
-    //         .distanceMin(15)
-    //         .distanceMax(20),
-    //     )
-    //     .force('x', d3.forceX())
-    //     .force('y', d3.forceY())
-    //     // .force('center', d3.forceCenter(data.svgW, data.svgH / 1.5))
-    //     .force(
-    //       'collide',
-    //       d3
-    //         .forceCollide(d => {
-    //           // if (d.name === this.currentWord && d.level === 1) {
-    //           //   d.fx = this.svgW / 1.1; // 设置特定节点固定x坐标
-    //           //   d.fy = this.svgH / 1.5;
-    //           // }
-    //           // return 120 - d.level * 14;
-    //           return 120;
-    //         })
-    //         .iterations(0.5)
-    //         .strength(0.5),
-    //     );
-
-    //   console.log(simulation);
-    console.log(data.nodes);
-    const nodeData = data.nodes;
-    simulation.nodes(nodeData).on('tick', (): void => {
-      edges.attr('x1', data => {
-        // console.log(data);
-        return data.source.x;
+    // 创建一个弹簧力，根据 link 的 strength 值决定强度
+    const linkForce = d3
+      .forceLink<INode, ILink>(linksData)
+      .id(data => {
+        // return "'" + data.code + "'";
+        return data.code.toString();
+      })
+      .distance(({ target }) => {
+        // 无分支的节点
+        // console.log(data)
+        if ((target as INode).name === '荣誉' || (target as INode).name === '组织') {
+          return 200;
+        } else {
+          return (target as INode).level === 5
+            ? (target as INode).level * 22
+            : (target as INode).level * 3;
+        }
       });
-      //   .attr('y1', data => data.source.y)
-      //   .attr('x2', data => data.target.x)
-      //   .attr('y2', data => data.target.y);
+
+    // 资源节点与信息节点间的 strength 小一点，信息节点间的 strength 大一点
+    // .strength(link => link.strength);
+
+    // 创建一个力导向图
+    const simulation = d3
+      .forceSimulation<INode, ILink>(nodesData)
+      .alpha(2) // 活力，渲染之后再自动动多久
+      .force('link', linkForce) // 映射id & 线的长度
+      .force(
+        'collide',
+        d3
+          .forceCollide(data => {
+            const d = data as INode;
+            if (d.name === '吴恩达' && d.level === 1) {
+              d.fx = svgData.svgW / 1.1; // 设置特定节点固定x坐标
+              d.fy = svgData.svgH / 1.5;
+            }
+            return 120 - d.level * 14;
+          })
+          .iterations(0.5)
+          .strength(0.5),
+      )
+      .force(
+        'charge',
+        d3
+          .forceManyBody()
+          // .strength(d => -(9 - d.level) * 30)
+          .theta(0.01)
+          .distanceMin(15)
+          .distanceMax(20),
+      );
+    simulation.nodes(nodesData).on('tick', () => {
+      edges
+        .attr('x1', ({ source }) => (source as INode).x || 0)
+        .attr('y1', ({ source }) => (source as INode).y || 0)
+        .attr('x2', ({ target }) => (target as INode).x || 0)
+        .attr('y2', ({ target }) => (target as INode).y || 0);
       nodes.attr('transform', data => `translate(${data.x}, ${data.y})`);
       // linkTagsSelection.attr(
       //   'd',
       //   d => 'M ' + d.source.x + ' ' + d.source.y + ' L ' + d.target.x + ' ' + d.target.y,
       // );
     });
-    // forceSimulation.force('link').links(data.links);
+    // simulation.force('link').links(linksData);
 
-    // setNodesSelection(nodes);
     // 绘制实体节点
     const nodes = d3
       .select('svg g')
       .append('g')
       .attr('class', 'nodes')
       .selectAll('.node')
-      .data(data.nodes)
+      .data(nodesData)
       .enter()
       .append('g')
       .attr('class', 'node')
@@ -147,17 +133,70 @@ const Graph: React.FC<IGraphProps> = (props: IGraphProps) => {
         // console.log(data);
         return data.name;
       });
+
+    // .call(d3.drag().on('start', this.started).on('drag', this.dragged).on('end', this.ended))
+    // .on('click', d => {
+    //   // this.clickNodeHandle(d);
+    // });
+
+    nodes
+      .append('circle')
+      .attr('class', 'circle-node')
+      // .attr('nodes', d => d)
+      .attr('r', d => svgData.rediusList[d.level])
+      .attr('fill', d => svgData.colorList[d.colorIdx])
+      .attr('stroke', '#fff')
+      .attr('stroke-width', '4px')
+      .attr('style', 'cursor: pointer;')
+      .attr('nodeTest', d => JSON.stringify(d))
+      .attr('transform', data => `translate(${data.x}, ${data.y})`);
+
+    nodes
+      // .filter(d => {
+      //   if (d.level && d.level < 4) {
+      //     return this;
+      //   }
+      // })
+      .append('text')
+      .attr('style', d => {
+        return `cursor: pointer; text-anchor: middle;font-size:${
+          svgData.fontSizeList[d.level - 1]
+        }px`;
+      })
+      .selectAll('tspan')
+      .data((d: INode): any => {
+        if (d.name) {
+          if (d.name.includes('.')) {
+            return d.name.split('.') || '';
+          } else {
+            return d.name.split(' ') || '';
+          }
+        }
+      })
+
+      .join('tspan')
+      .attr('fill', '#f1f1f1')
+      .attr('x', 0)
+      .attr('y', (d, i, nodes) => {
+        if (nodes) {
+          return `${i - nodes.length / 2 + 1.8}em`;
+        } else {
+          return `0em`;
+        }
+      })
+      .text(name => name as string);
+
     // 绘制关系线
     const edges = d3
       .select('svg g')
       .append('g')
       .attr('class', 'edges')
       .selectAll('line')
-      .data(data.links)
+      .data(linksData)
       .enter()
       .append('line')
       .attr('class', 'edge')
-      .attr('stroke', 'rgba(255,255,255,0.6)')
+      .attr('stroke', 'red')
       .attr('stroke-width', '2px');
     // .style('display', 'none');
   };
@@ -179,7 +218,7 @@ const Graph: React.FC<IGraphProps> = (props: IGraphProps) => {
   //     .attr('name', data => {
   //       console.log(data);
   //       return data.name;
-  //     });
+  //     })
 
   // .attr('transform', data => `translate(${data.x}, ${data.y})`)
   // .call(d3.drag().on('start', this.started).on('drag', this.dragged).on('end', this.ended))
